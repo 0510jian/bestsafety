bf5273c33d64e8e6939855cb467db75c73a3e33d
0510jian <0510jian@gmail.com>
2024-02-21 20:31:40 +0900

    feat(data): add date format parsing exception handling

--- src/main/java/com/bestsafety/bs/data/DataService.java
+++ src/main/java/com/bestsafety/bs/data/DataService.java
@@ -1,7 +1,9 @@
 package com.bestsafety.bs.data;
 
 import com.bestsafety.bs.dto.Content;
-import org.hibernate.query.Query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +16,8 @@ import java.util.Date;
 
 @Service
 public class DataService {
+    private static final Logger log = LoggerFactory.getLogger(DataService.class);
+
     @Autowired
     private com.bestsafety.bs.repository.ContentRepository contentRepository;
 
@@ -29,11 +33,25 @@ public class DataService {
         return contents;
     }
 
+    /*
+     * 날짜/시간을 지정된 format으로 변환하는 함수
+     *
+     * @param beforeDate(변환할 날짜/시간), format(변환하고 싶은 포맷)
+     * @throws NullPointerException | ParseException beforeDate의 값이 변환 불가능한 값일 경우 예외 발생
+     */
     public String changeDateFormat(String beforeDate, String format) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-        Date date = simpleDateFormat.parse(beforeDate);
 
-        String afterDate = simpleDateFormat.format(date);
+        Date date;
+        String afterDate;
+
+        try {
+            date = simpleDateFormat.parse(beforeDate);
+            afterDate = simpleDateFormat.format(date);
+        } catch (NullPointerException | ParseException e) {
+            log.warn(e.getMessage());
+            afterDate = "미기재";
+        }
 
         return afterDate;
     }
