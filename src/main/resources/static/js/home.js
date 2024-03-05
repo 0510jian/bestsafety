2635f060481c406a5e87c52024a4236e453cbeb7
0510jian <0510jian@gmail.com>
2024-03-05 21:17:29 +0900

    design(header): modify header layout

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -2,6 +2,7 @@ window.onload = function() {
     showSlide(1);
     startAutoSlide();
     cloneRoller();
+    resizeHeaderMenuItem();
 }
 
 let slideIndex = 1;
@@ -57,5 +58,18 @@ function cloneRoller() {
     for (var i = 0; i < childrenLength; i++) {
         var clonedChild = children[i].cloneNode(true);
         roller.appendChild(clonedChild);
+    }
+}
+
+function resizeHeaderMenuItem() {
+    var logo = document.querySelector('#logo');
+    var header = document.querySelector('.header_area');
+    var menu = document.querySelector('.menu').childElementCount;
+
+    var length = ((header.offsetWidth - logo.offsetWidth) - (header.offsetWidth / 10)) / menu;
+    var menuItems = document.querySelectorAll('.menu_item');
+
+    for(var i=0; i<menuItems.length; i++) {
+        menuItems[i].style.width = length + "px";
     }
 }
